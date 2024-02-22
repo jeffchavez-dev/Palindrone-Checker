8f30335b1a659dea2c1f3d167a916b572332c466
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-22 21:51:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const check = document.getElementById("check-btn")
 const input = document.getElementById("text-input")
+const result = document.getElementById("result")
 
 check.addEventListener('click', () => {
    let inputValue = input.value
+   let lowerCase = inputValue.toLowerCase()
+   
+   function removeSpecialCharacter(str){
+        return str.replace(/[^\w\s]/gi, '');
+   }
+   const okay = "okay okay"
+   console.log(removeSpecialCharacter(okay))
+
 
    function reverseString(str) {
     // Split the string into an array of characters, reverse the array, and join it back into a string
-    return str.split('').reverse().join('');
-  }
-   switch (inputValue) {
+        return str.split('').reverse().join('');
+    }
+   switch (lowerCase) {
         case "":
-            alert("Please input a value")
+            result.textContent = "Please input a value"
             break;
-        case reverseString(inputValue):
-            alert(`${inputValue} is a palindrome`)
+        case reverseString(lowerCase):
+            result.textContent = `${inputValue} is a palindrome`
             break;
     default:
-        alert(`${inputValue} is not a palindrome`)
+        result.textContent = `${inputValue} is not a palindrome`
    }
 })
