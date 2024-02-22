56b5504f5ca66409b89175d2f1be6d35594ee310
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-22 21:36:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const check = document.getElementById("check-btn")
 const input = document.getElementById("text-input")
 
 check.addEventListener('click', () => {
-   if (input.value === "") {
-        alert("Please input a value")
+   let inputValue = input.value
+
+   function reverseString(str) {
+    // Split the string into an array of characters, reverse the array, and join it back into a string
+    return str.split('').reverse().join('');
+  }
+   switch (inputValue) {
+        case "":
+            alert("Please input a value")
+            break;
+        case reverseString(inputValue):
+            alert(`${inputValue} is a palindrome`)
+            break;
+    default:
+        alert(`${inputValue} is not a palindrome`)
    }
 })
